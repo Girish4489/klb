2748061f383af541ca18253fd69d1bc36518d759
Girish Kumar <girishkumar4489@gmail.com>
2023-12-31 14:33:11 +0530

    Add favicon and metadata to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,16 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/logo/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/logo/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/logo/favicon-16x16.png" />
+        <link rel="mask-icon" href="/icons/logo/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="manifest" href="/icons/logo/site.webmanifest" />
+        <meta name="msapplication-config" content="/icons/logo/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
+
       <ThemeProvider>
         <body className={`${inter.className}`}>{children}</body>
       </ThemeProvider>
