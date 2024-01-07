df4b03a305cae00860cfe0e39f468bfc30902d08
Girish4489 <girishkumar4489@gmail.com>
2024-01-07 14:54:31 +0530

    Update ThemeProvider import in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import { ThemeProvider } from '@/app/components/ThemeContext';
 import TopbarLoader from '@/app/components/topbarLoader/page';
+import { ThemeProvider } from '@/app/context/ThemeContext';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="">
+    <html lang="en">
       <head>
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/logo/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/logo/favicon-32x32.png" />
