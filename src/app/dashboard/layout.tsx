78ebc35a0ba865f078c41bc8e807a70f0042e759
Girish Kumar <girishkumar4489@gmail.com>
2024-03-05 21:42:10 +0530

    Refactor profile page and add user profile image to dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,22 +1,100 @@
 'use client';
-import ProfilePage from '@/app/components/profile/page';
+import LogoutPage from '@/app/components/logout/page';
 import Sidebar from '@/app/components/sidebar/page';
+import { useUser } from '@/app/context/userContext';
+import { IUser } from '@/models/userModel';
+import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
-export default function AuthLayout({
-  children, // will be a page or nested layout
-}: {
-  children: React.ReactNode;
-}) {
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const { fetchAndSetUser, user } = useUser();
   useEffect(() => {
     document.title = 'Dashboard | Kalamandir';
-  }, []);
+    fetchAndSetUser();
+  }, [fetchAndSetUser]);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   let startX = 0;
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const ProfilePage = ({ user }: { user: Omit<IUser, 'password'> }) => {
+    return (
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="avatar">
+          <div className="w-12 transform rounded-full ring-2 ring-primary hover:scale-105 hover:ring-offset-2 hover:ring-offset-accent">
+            {/* src if Uint8Array */}
+            <Image
+              src={
+                user.profileImage.__filename !== 'USER_PROFILE_404_ERROR'
+                  ? `data:${user.profileImage.contentType};base64,${Buffer.from(user.profileImage?.data || []).toString('base64')}`
+                  : '/klm.webp'
+              }
+              alt="profile image"
+              className="cursor-pointer rounded-full transition-all duration-500 ease-in-out"
+              width="40"
+              height="40"
+              priority
+            />
+          </div>
+        </div>
+        <ul tabIndex={0} className="menu dropdown-content menu-sm z-50 mt-3 w-auto rounded-box bg-base-200 p-2 shadow">
+          <li className="tooltip tooltip-left" data-tip="Username">
+            <a className="justify-between">
+              {user.username}
+              {user.createdAt && new Date(user.createdAt) > new Date(Date.now() - 2 * 24 * 60 * 60 * 1000) && (
+                <span className="badge badge-primary">New</span>
+              )}
+            </a>
+          </li>
+          <li className="tooltip tooltip-left" data-tip="Email">
+            <a>{user.email}</a>
+          </li>
+          <li>
+            <a className="justify-between">
+              Verified
+              <span className={`badge ${user.isVerified ? 'badge-success' : 'badge-error'}`}>
+                {user.isVerified ? 'Yes' : 'No'}
+              </span>
+            </a>
+          </li>
+          <li>
+            <a className="justify-between">
+              Admin
+              <span className={`badge ${user.isAdmin ? 'badge-success' : 'badge-error'}`}>
+                {user.isAdmin ? 'Yes' : 'No'}
+              </span>
+            </a>
+          </li>
+          <li>
+            <span className="justify-between">
+              Theme
+              <Link
+                href="/dashboard/settings#themeBlock"
+                className="tooltip tooltip-top flex flex-row gap-2 rounded-box bg-primary pl-2 pr-1 font-medium text-primary-content"
+                data-tip="edit theme"
+              >
+                {user.theme.charAt(0).toUpperCase() + user.theme.slice(1)}
+                <span className="badge badge-secondary">
+                  <Image src="/icons/svg/note-pencil.svg" alt="edit icon" width="18" height="18" />
+                </span>
+              </Link>
+            </span>
+          </li>
+          <li className={`tooltip tooltip-left`} data-tip="Settings">
+            <Link href="/dashboard/settings">Settings</Link>
+          </li>
+          <li
+            className="tooltip tooltip-left text-warning hover:rounded-lg hover:bg-error hover:font-medium hover:text-warning-content"
+            data-tip="Logout"
+          >
+            <LogoutPage />
+          </li>
+        </ul>
+      </div>
+    );
   };
 
   return (
@@ -56,7 +134,7 @@ export default function AuthLayout({
         </div>
 
         {/* Profile dropdown */}
-        <ProfilePage />
+        <ProfilePage user={user} />
       </div>
       {/* sidebar and content page */}
       <div
