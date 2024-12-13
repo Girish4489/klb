58dd8d77e3f8722e07066e5b556301cea399faa3
Girish4489 <girishkumar4489@gmail.com>
2024-12-13 15:52:57 +0530

    Enhance notifications component styling: update icons to use primary color and add a ring effect to the dropdown for improved visibility

--- src/app/dashboard/components/Notifications.tsx
+++ src/app/dashboard/components/Notifications.tsx
@@ -10,16 +10,19 @@ const Notifications = ({ notifications }: { notifications: INotification[] }) =>
         {/* Notifications icon */}
         {notifications.length > 1 ? (
           <div className="indicator">
-            <BellSlashIcon className="h-6 w-6" />
+            <BellSlashIcon className="h-6 w-6 text-primary" />
           </div>
         ) : (
           <div className="indicator">
-            <BellIcon className="h-6 w-6" />
+            <BellIcon className="h-6 w-6 text-primary" />
             {unreadCount > 0 && <span className="badge indicator-item badge-sm">{unreadCount}</span>}
           </div>
         )}
       </div>
-      <div tabIndex={0} className="card dropdown-content card-compact z-[1] mt-3 w-52 bg-base-100 shadow">
+      <div
+        tabIndex={0}
+        className="card dropdown-content card-compact z-[1] mt-3 w-52 bg-base-100 shadow ring-1 ring-primary"
+      >
         <div className="card-body">
           <span className="text-lg font-bold">{notifications.length} Notifications</span>
           <span className="text-info">View all notifications</span>
